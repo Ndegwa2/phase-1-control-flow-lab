8c9c5021a937be458877fa155d47205e5bf33bcb
Ndegwa <ndegwas03@gmail.com>
2024-04-11 10:38:53 +0300

    corrections made

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if(feet <= 400){
+    return "Free Ride";
+  }else if(feet > 400 && feet <= 2000){
+    return "Twenty bucks please";
+  }else if(feet >2000 && feet <=2500){
+    return "Thirty bucks please";
+  }else {
+    return "Too Far"
+  }
+}
+
+module.exports = {
+  scuberGreetingForFeet
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  return city === "NYC"? "ok, that will do": "No can't do";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case "generous":
+      return "Thank you.";
+      case"not generous":
+      return "Thank you";
+      default:
+        return "See you!";
+  }
+  
+  
 }
+module.exports ={
+  scuberGreetingForFeet,
+  ternaryCheckCity,
+  switchOnCharmFromTip
+};
