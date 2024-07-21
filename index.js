00a05b67cf655d8734c3c9242f1c0dde38b5df08
Ndegwa2 <138137867+Ndegwa2@users.noreply.github.com>
2024-07-21 19:58:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-function scuberGreetingForFeet(){
-  if(feet <= 400){
-    return "Free Ride";
-  }else if(feet > 400 && feet <= 2000){
-    return "Twenty bucks please";
-  }else if(feet >2000 && feet <=2500){
-    return "Thirty bucks please";
-  }else {
-    return "Too Far"
+
+
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 400 && someValue <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someValue > 2500) {
+    return "No can do.";
   }
 }
 
-module.exports = {
-  scuberGreetingForFeet
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  return city === "NYC"? "ok, that will do": "No can't do";
-
-}
-
-function switchOnCharmFromTip(tip){
-  switch (tip){
-    case "generous":
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case 'not as generous':
       return "Thank you.";
-      case"not generous":
-      return "Thank you";
-      default:
-        return "See you!";
+    default:
+      return "Bye.";
   }
-  
-  
 }
-module.exports ={
-  scuberGreetingForFeet,
-  ternaryCheckCity,
-  switchOnCharmFromTip
-};
